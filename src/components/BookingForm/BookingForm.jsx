006c097b6bacee27105625a0ac314f1d3991ca0d
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-01-06 23:38:43 +0600

    booking form done

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,12 +1,183 @@
+import { TextField } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import {Controller, useForm } from "react-hook-form";
+import Select from 'react-select'
+// import { AuthContext } from "../../../provider/AuthProvider";
+
+// import axios from "axios";
+// import Swal from "sweetalert2";
 
 
-const BookingForm = () => {
+import { AuthContext } from "../../provider/AuthProvider";
+import { DatePicker } from "antd";
+import { AwesomeButton } from "react-awesome-button";
+
+import './BookingForm.css'
+import axios from "axios";
+import Swal from "sweetalert2";
+
+
+const BookingForm= ({price}) => {
+const {user}=useContext(AuthContext)
+const[date,setDate]=useState(null)
+const [disable,setDisable]=useState(true)
+console.log(price)
+    const { register, handleSubmit, reset, formState: { errors },setError,control } = useForm();
+    const options = [
+        { value: 'x', label: 'x' },
+        { value: 'y', label: 'y' },
+        { value: 'z', label: 'z' }
+      ]
+      const onChange = (date, dateString) => {
+        console.log(date, dateString);
+        setDate(dateString)
+      }
+    useEffect(()=>{
+        if (user){
+            setDisable(false)
+        }
+        else{
+            setDisable(true)
+        }
+
+      },[user])
+    const onSubmit=async (data)=>{
+        console.log(data)
+        const book={name:data.name, photo:data.photo,tourDate:date,email:user.email,guide:data.guide.value,prices:price}
+        console.log(book,"from book now")
+        const bookData = await axios.post('http://localhost:5000/booking', book)
+            console.log(bookData.data)
+            if(bookData.data.insertedId){
+                
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Booking confirmed`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+                
+            }
+
+
+    }
+    
     return (
-        <div>
-            <h2>form</h2>
+
+
+<div className="flex justify-center mx-auto mt-3">
+
+    <div className=" bg-white w-[400px] rounded-2xl p-4 shadow-2xl shadow-slate-400">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control  ">
+                        <div>
+                        
+                    <TextField
+                
+                id="outlined-search1"
+                label="Tourist Name"
+                type="text"
+                {...register("name", { required: true })}
+                name="name"
+                placeholder="Enter your name.."
+                InputProps={{
+                    style: {
+                    color: 'black',
+                    borderBottom: "1px solid black",
+                    },
+                    
+                }}
+                style={{ fontFamily: 'serif', width: '100%' }}
+                variant="standard"  
+                />
+
+                        </div>
+            </div>
+            <div className="form-control  ">
+                        <div>
+                        
+                        <TextField
+                        id="outlined-search2"
+                        label="Tourist Email"
+                        type="text"
+                      
+                        {...register("email", { required: true })}
+                        name="email"
+                        InputProps={{
+                            style: {
+                            color: 'black',
+                            borderBottom: "1px solid black",
+                            },
+                        }}
+                        style={{ fontFamily: 'serif', width: '100%' }}
+                        variant="standard"
+                        />
+
+
+                        </div>
+            </div>
+            <div className="form-control  ">
+            <div>
+                        
+                        <TextField
+                        id="outlined-search2"
+                        label="Image"
+                        type="text"
+                      
+                        {...register("photo", { required: true })}
+                        name="photo"
+                        InputProps={{
+                            style: {
+                            color: 'black',
+                            borderBottom: "1px solid black",
+                            },
+                        }}
+                        style={{ fontFamily: 'serif', width: '100%' }}
+                        variant="standard"
+                        />
             
-            
+            </div>
+            </div>
+            <div className="form-control mt-3">
+            <Controller
+          name="guide"
+          control={control}
+          {...register("guide", { required: true })}
+          // Set your default value here
+          render={({ field }) => (
+            <Select
+              {...field}
+              defaultValue={{label:"select tour guide"}}
+              options={options} // Provide your options array
+            />
+          )}
+        />
+
+
+
+            </div>
+            <div className=" flex justify-between mt-2">
+            <div className="mt-1">
+        <DatePicker   onChange={onChange} />
         </div>
+        <div className="mt-2">
+       <p className=" font-serif ">Price: {price}</p>
+        </div>
+            </div>
+
+            <div className="form-control mt-2">
+                                <input disabled={disable} className="btn bg-[#4a9bb4] font-Sora font-semibold" type="submit" value="Book Now" />
+                                
+                            </div>
+        
+        </form>
+    </div>
+
+
+       
+   </div>
+
     );
 };
 
--- src/pages/PackageDetails/PackageDetails.jsx
+++ src/pages/PackageDetails/PackageDetails.jsx
@@ -5,7 +5,10 @@ import { useParams } from "react-router-dom";
 import { HiMiniCalendarDays } from "react-icons/hi2";
 import { BiSolidPlaneAlt } from "react-icons/bi";
 import BookingForm from "../../components/BookingForm/BookingForm";
-
+import './PackageDetails.css'
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const PackageDetails = () => {
     const {id}=useParams()
@@ -16,6 +19,12 @@ const PackageDetails = () => {
             return res.data;
         }
     })
+    useEffect(() => {
+      Aos.init({
+          duration:1500
+      });
+      Aos.refresh();
+    }, []);
     if(!isFetched){
         return <div className='flex justify-center items-center min-h-screen place-content-center mx-auto place-items-center '><Watch
         height="80"
@@ -32,7 +41,7 @@ const PackageDetails = () => {
     const{image,image2,image3,image4,image5,title,description,duration,itinerary,location,price,tour_type}=packages[0]
     console.log(image)
 
-    
+
     
 
     return (
@@ -40,7 +49,8 @@ const PackageDetails = () => {
             <div className="relative">
                 <img src={image} alt="" className="lg:h-[700px] md:h-[700px] h-[400px] w-full object-fill"  />
                 <div className="absolute inset-0 bg-gradient-to-r from-slate-800 to-transparent"></div>
-                <div className="lg:text-3xl md:text-3xl text-xl font-extrabold text-white absolute z-20 left-5 bottom-[50%]">
+                <div className="lg:text-3xl md:text-3xl text-xl font-extrabold text-white absolute z-20 left-5 bottom-[50%]"  data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom">
                     <p>{title}</p>
                     <p>{location}</p>
                 </div>
@@ -55,7 +65,7 @@ const PackageDetails = () => {
                     <div className="lg:text-xl md:text-[15px] text-[14px] mt-4 font-serif font-semibold flex  ">Duration:  <span className="font-medium">{duration}</span><div className="mt-1 ml-2"><HiMiniCalendarDays></HiMiniCalendarDays></div></div>
                     </div>
                     <div className="carousel lg:w-[70%] mt-8 lg:mt-8 md:mt-4 font-serif">
-  <div id="slide1" className="carousel-item relative w-full">
+  <div id="slide1" className="carousel-item relative w-full ">
   <div className="text-center bg-blue-100 w-full "><div className="flex justify-center pt-[11%] font-semibold ">Day-1<div className="mt-1 ml-2 "><BiSolidPlaneAlt></BiSolidPlaneAlt></div></div><p className="pb-[11%] ">{itinerary[0]}</p></div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle btn-ghost">❮</a> 
@@ -90,7 +100,8 @@ const PackageDetails = () => {
 
                 </div>
 
-                <div className="lg:w-[30%] md:w-[40%] md:flex md:flex-col  md:place-content-center">
+                <div className="lg:w-[30%] md:w-[40%] md:flex md:flex-col  md:place-content-center"  data-aos="zoom-in"
+>
                     <div className="flex gap-2">
                         <div>
                             <img src={image2} alt="" className="h-[160px] w-[200px]" />
@@ -120,9 +131,10 @@ const PackageDetails = () => {
             </div>
             <div>
               <h2 className="text-center text-yellow-500 text-4xl mt-16">BOOK NOW</h2>
-            <div className="flex bg-slate-300 h-[800px]">
+              <hr className="text-center h-1 w-[50%] bg-[#edbd4c] justify-center mx-auto rounded-full mt-3 "></hr>
+            <div className="backg flex  py-10">
              
-              <BookingForm></BookingForm>
+              <BookingForm price={price}></BookingForm>
 
           
 
